4943487ca3301e1621b83f7f1680318bcba8a07d
bradley <bradley.pemmelaar@my.jcu.edu.au>
2018-10-06 10:41:46 +1000

    requiring API key for open weather map

--- weather-app/server.js
+++ weather-app/server.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+const apiKey = '*****************';
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index');
 })
 
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weatherq=${city}&units=imperial&appid=${apiKey}`
+    console.log(req.body.city);
+
+      request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
 app.listen(8000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8000!')
 })
