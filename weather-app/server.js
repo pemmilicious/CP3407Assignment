d6f442961bc424ca1297ddc84ed43597c6c5bace
bradley <bradley.pemmelaar@my.jcu.edu.au>
2018-10-29 17:13:53 +1000

    database

--- weather-app/server.js
+++ weather-app/server.js
@@ -3,24 +3,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
+const mysql = require('mysql')
 
-////MySQL Connection
-//var mysql = require('mysql');
-//var connect = mysql.createConnection({
-//    //properties...
-//    host: 'localhost',
-//    user: 'root',
-//    password: '',
-//    database: 'weatherDB'
-//});
+//MySQL Connection
+const db = mysql.createConnection({
+  host:'localhost',
+  user: 'root',
+  password: ''
+});
 
-//connection.connect(function(error){
-//    if(!!error) {
-//        console.log('Error');
-//    } else {
-//        console.log('Mysql connection Established!')
-//    }
-//});
+//connect
+db.connect((err) => {
+  if(err){
+    console.log('Error Connection')
+  } 
+  console.log('MySQL Connected....')
+});
+
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE weather'
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('database created....');
+
+  });
+});
 
 
 const apiKey = 'e44c166edbf14fc31a59ead146573952';
@@ -29,8 +37,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+app.get('/', function (req, res, resp) {
+    res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
@@ -45,23 +53,35 @@ app.post('/', function (req, res) {
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        let tempText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: tempText, error: null});
       }
     }
   });
 })
+ 
+//create the weather table
+app.get('createweathertable', (req, res) =>{
+  let sql = 'CREATE TABLE weather(id int AUTO_INCREMENT, Temp int, Humidity int, PRIMARY KEY (id))';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('weather table created')
+  });
+});
 
-//app.get('/', function(req, resp){
-//    // about sql DB
-//    connection.query("SELECT * FROM weatherDB", function(error, rows, fields)
-//    if(!!error){
-//        console.log('Error in Query')
-//    }else {
-//        console.log('Successful Query!')
-//    }
-//}));
+//insert data into table
+app.get('/addData1', (req, res)=> {
+  let post = {Temp: weather.main.temp, Humidity: weather.main.humidity};
+  let sql = 'INSERT INTO weather SET ?';
+  let query = db.query(sql, post, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('weather data added');
+  });
+
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Server started on port 3000!')
 })
